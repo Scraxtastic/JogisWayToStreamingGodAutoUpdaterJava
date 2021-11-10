9507b1a977e3bb637910607bf78c6b5d09f89145
Scraxtastic <nick.u19990@gmail.com>
2021-11-11 00:21:15 +0100

    Removed unused imports

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,14 +1,4 @@
-import javax.naming.spi.DirectoryManager;
 import java.io.*;
-import java.net.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 public class Launcher {
     public static void main(String[] args) {
@@ -22,7 +12,7 @@ public class Launcher {
         String gamePath = gameFolderName + "/JogisWayToStreamingGod.exe";
         Runtime runtime = Runtime.getRuntime();
         try {
-            Process process = runtime.exec("cmd /c start " + gamePath);
+           runtime.exec("cmd /c start " + gamePath);
         } catch (IOException e) {
             e.printStackTrace();
         }
